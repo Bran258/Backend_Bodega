abcc482032fe30c76bd14bb9887e5100eda537b3
Bran258 <233349187+Bran258@users.noreply.github.com>
2025-10-17 21:15:08 -0500

    Actualizar productos y ventas

---
+++ backend/src/routes/ventas/ventas.js
@@ -0,0 +1,15 @@
+// routes/ventas/ventas.js
+const express = require('express');
+const router = express.Router();
+const ventasProductosController = require('../../controllers/ventasProductosController');
+
+// === Rutas para productos de las ventas ===
+
+router.get('/lista', ventasProductosController.obtenerVentasProductos);
+router.get('/producto/:id', ventasProductosController.obtenerVentaProductoPorId);
+router.post('/crear_venta', ventasProductosController.crearVenta);
+router.put('/actualizar_producto/:id', ventasProductosController.actualizarProductoEnVenta);
+router.patch('/productos/:id', ventasProductosController.desactivarVenta);
+
+
+module.exports = router;
